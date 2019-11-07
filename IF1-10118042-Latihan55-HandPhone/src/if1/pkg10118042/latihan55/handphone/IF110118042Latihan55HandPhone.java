44b45e45682e17a9b4b51378ede9846b6f267eaa
DhimasAjiG <55832635+DhimasAjiG@users.noreply.github.com>
2019-11-07 20:01:20 +0700

    Add files via upload

---
+++ IF1-10118042-Latihan55-HandPhone/src/if1/pkg10118042/latihan55/handphone/IF110118042Latihan55HandPhone.java
@@ -0,0 +1,30 @@
+
+package if1.pkg10118042.latihan55.handphone;
+
+/**
+ * @author
+ * Nama  : Dhimas Aji Ghairrahmat
+ * Kelas : IF1
+ * NIM   : 10118042
+ * Deskripsi Program : Program ini berfungsi untuk Menampilkan Informasi tentang HandPhone
+ */
+public class IF110118042Latihan55HandPhone {
+
+    public static void main(String[] args) {
+        Android android = new Android("Samsung", "Android", "Grand", 3000000);
+        android.displayProduct();
+        android.setKeyStore("234ibfd3840fo");
+        System.out.println("Andriod Key Store : " + android.getKeyStore());
+
+        BlackBerry blackBerry = new BlackBerry("BlackB", "RIM", "Curve", 2000000);
+        blackBerry.displayProduct();
+        blackBerry.setPinBB("BHS249");
+        System.out.println("PIN : " + blackBerry.getPinBB());
+
+        WindowsPhone windowsPhone = new WindowsPhone("Nokia", "Win8", "Lumia", 1000000);
+        windowsPhone.displayProduct();
+        windowsPhone.setWpKeyStore("UUUQIJWORJ");
+        System.out.println("WP Key Store : " + windowsPhone.getWpKeyStore());
+    }
+    
+}
